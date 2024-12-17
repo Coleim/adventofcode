6e6e87265002e75e6edfe610c6cd13f786260994
Clément Oliva <clement.oliva@gmail.com>
2024-12-17 22:40:26 +0100

    Day 10

---
+++ challenge2024/src/day10.2.ts
@@ -0,0 +1,66 @@
+
+import { readFileSync } from 'fs';
+const file = readFileSync('inputs/day10', 'utf8');
+
+interface Position {
+  x: number;
+  y: number;
+}
+const map: number[][] = [];
+const startingPoints: Position[] = []
+let y = 0
+file.split(/\r?\n/).forEach(function(line) {
+  if (line.length === 0) return
+  const cols = line.split('').map(s => Number(s))
+  map.push(cols)
+  cols.map((e, i) => e === 0 ? i : null).filter(s => s !== null).forEach(s => startingPoints.push({ x: s, y }))
+  ++y
+})
+
+console.log("> Map: ", map)
+console.log("> Starting Points: ", startingPoints)
+
+let trailheads = 0
+
+startingPoints.forEach(p => {
+  let t = walk(p, map)
+  trailheads += t
+})
+
+console.log("Trail : ", trailheads)
+
+// console.log("> Map: ", map)
+
+function walk(p: Position, map: number[][]) {
+
+  const weight = map[p.y][p.x]
+  // if w == 9 => it is the end, so add 1
+  if (weight === 9) {
+    return 1
+  }
+
+  let pathFounds = 0
+
+  // Search bigger weight around
+  if (map[p.y - 1] !== undefined && map[p.y - 1][p.x] === weight + 1) {
+    pathFounds += walk({ x: p.x, y: p.y - 1 }, map)
+
+  }
+  if (map[p.y + 1] !== undefined && map[p.y + 1][p.x] === weight + 1) {
+    pathFounds += walk({ y: p.y + 1, x: p.x }, map)
+  }
+  if (map[p.y][p.x + 1] === weight + 1) {
+    pathFounds += walk({ y: p.y, x: p.x + 1 }, map)
+  }
+  if (map[p.y][p.x - 1] === weight + 1) {
+    pathFounds += walk({ y: p.y, x: p.x - 1 }, map)
+  }
+
+
+  return pathFounds
+
+
+}
+
+
+
